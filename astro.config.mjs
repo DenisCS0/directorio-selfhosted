7ea929f9645f99c12a938071c80a50aa25962a61
DenisCS0 <decasa10@gmail.com>
2026-09-21 00:17:55 +0200

    feat: se quita la barra final de las url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://selfhostdeck.com',
+  trailingSlash: 'never',
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'es',
